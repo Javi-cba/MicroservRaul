b43a2d57c3b6e58457fed3bbd13b38490e931fb8
Javi-cba <cordobajava@gmail.com>
2024-04-17 21:11:43 -0300

    Guardando cambios locales

--- Index.js
+++ Index.js
@@ -11,8 +11,9 @@ const PORT = 3500
 app.get('/Peliculas', async(req, res) => {
     
     try {
-        const resp = await axios.get(`http://localhost:OtroPuerto/listMovies`);// reemplazalo con el endpoint
-        res.json(resp.data);// aca deberia mostrar el json que devuelve la api de peliculas
+        const resp = await axios.get(`https://dictionaryapi.com/api/v3/references/spanish/json/Correr?key=47f65980-f725-42b4-8505-2b1c1967450b`);// reemplazalo con el endpoint
+        const cjtsArray = resp.data[0].suppl.cjts; 
+        res.json(cjtsArray);// aca deberia mostrar el json que devuelve la api de peliculas
      
     } catch (error) {
       console.error(error);
